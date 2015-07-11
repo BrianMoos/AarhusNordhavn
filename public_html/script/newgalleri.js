05a76cf45571a4d239a5d8b1c0c81788b4b4941d
Jakob Dahl <jdah@via.dk>
2015-07-11 16:38:02 +0200

    refactoring js in new galleri

--- public_html/script/newgalleri.js
+++ public_html/script/newgalleri.js
@@ -3,16 +3,16 @@
     $(document).ready(function(){
         var baseURL = "./galleri/";
         $.getJSON("./galleri/galleri.json").done(function(data){
-            $("#galleri").html(listThumbs(data));
+            $("#galleri").html(listThumbs(data, "thumb"));
         });//getJSON
     });//document ready
     
-    function listThumbs(data){
+    function listThumbs(data, size){
         var baseURL = "./galleri/";
         var html = "";
-        html = html.concat('<ul id="gallerilist" class="thumbs">');
+        html = html.concat('<ul id="gallerilist" class="', size, '">');
         data.forEach(function(element){
-           html = html.concat('<li><img id="', element.id, '" src="', baseURL, element.thumb, '" /></li>') 
+           html = html.concat('<li><img id="', element.id, '" src="', baseURL, element[size], '" /></li>') 
         });
         html = html.concat('</ul>');
         return html;
