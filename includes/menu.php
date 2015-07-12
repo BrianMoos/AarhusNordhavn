dccad208596df84cbbdd62fbec5f9eb81f17ec09
Jakob Dahl <jdah@via.dk>
2015-07-12 19:26:24 +0200

    minor touchup

--- includes/menu.php
+++ includes/menu.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-default" role="navigation">
    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#example-navbar-collapse">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
          <span class="sr-only">Skift navigation</span>
          <span class="icon-bar"></span>
          <span class="icon-bar"></span>
@@ -8,15 +8,15 @@
       </button>
        <a class="navbar-brand" href="./index.php"><img id="navbar-image" src="./images/logo_blue.gif" /></a>
    </div>
-   <div class="collapse navbar-collapse" id="example-navbar-collapse">
+   <div class="collapse navbar-collapse" id="navbar-collapse">
       <ul class="nav navbar-nav navbar-right">
          <li id="index"><a href="index.php">Aarhus Nordhavn</a></li>
-		 <li id="gaestesejlere"><a href="./gaestesejlere.php">G&aelig;stesejlere</a></li>
-		 <li id="havnen"><a href="./havnen.php">Havnen</a></li>
-		 <li id="sejlklubben"><a href="./sejlklubben.php">Sejlklubben</a></li>
-		 <li id="pladser"><a href="./pladser.php">Pladser</a></li>
-		 <li id="fotogalleri"><a href="./fotogalleri.php">Fotogalleri</a></li>
-		 <li id="links"><a href="./links.php">Links</a></li>
+        <li id="gaestesejlere"><a href="./gaestesejlere.php">G&aelig;stesejlere</a></li>
+        <li id="havnen"><a href="./havnen.php">Havnen</a></li>
+        <li id="sejlklubben"><a href="./sejlklubben.php">Sejlklubben</a></li>
+        <li id="pladser"><a href="./pladser.php">Pladser</a></li>
+        <li id="fotogalleri"><a href="./fotogalleri.php">Fotogalleri</a></li>
+        <li id="links"><a href="./links.php">Links</a></li>
       </ul>
    </div>
 </nav>
