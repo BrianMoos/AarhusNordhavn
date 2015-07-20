fe9ab2ffe75985ea7eb8f36b9b1f5dd68061c2bf
Jakob Dahl <jakobdahl@gmail.com>
2015-07-21 01:48:44 +0200

    list galleri complete

--- public_html/script/galleri.js
+++ public_html/script/galleri.js
@@ -2,42 +2,21 @@
     "use strict";
     $(document).ready(function(){
         $.getJSON("./galleri/galleri.json").done(function(data){
-            $("#galleri").html(listThumbs(data, "thumb"));
-            addImgHandler(data); 
-            $("#back").click(function(){
-                $("#topimg").html('<img src="images/fotogalleri_top.png" alt="" class="center-block" />');
-                $("#galleri").html(listThumbs(data, "thumb"));
-                $("#image").html("");
-                addImgHandler(data); 
+            data.forEach(function(image){
+               $("#galleri").append(listGalleri(image)); 
             });
         });//getJSON
     });//document ready
     
-    function listThumbs(data, size){
-        var baseURL = "./galleri/";
+    function listGalleri(image){
         var html = "";
-        html = html.concat('<ul id="gallerilist" class="', size, '">');
-        data.forEach(function(element){
-           html = html.concat('<li><img id="', element.id, '" src="', baseURL, element[size], '" /></li>') 
-        });
-        html = html.concat('</ul>');
-        return html;
-    }
-    
-    function addImgHandler(data){
-        data.forEach(function(image){
-            $("#" + image.id).click(function(){
-               $("#topimg").html(""); 
-               $("#galleri").html(listThumbs(data, "micro"));
-               showImg(data, image.name);
-            });
-        });
-    }
-    
-    function showImg(data ,img){
         var baseURL = "./galleri/";
-        $("#image").html('<img src="' + baseURL + img + '" alt="" class="large_img img-responsive" />');
-        addImgHandler(data);
+        html = html.concat('<div class="col-sm-6 col-md-3">');
+        html = html.concat('<div class="thumbnail">');
+        html = html.concat('<img class="img-responsive" src="', baseURL, image.name, '" alt="" data-toggle="modal" data-target="#myModal">');
+        html = html.concat('</div>');
+        html = html.concat('</div>');
+        return html;
     }
     
 }(jQuery));
