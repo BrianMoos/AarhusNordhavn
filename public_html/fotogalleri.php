06abe993a4fae3bb4639f27c552ffe6c82a0056e
Jakob Dahl <jakobdahl@gmail.com>
2015-07-21 01:28:44 +0200

    add basic html

--- public_html/fotogalleri.php
+++ public_html/fotogalleri.php
@@ -10,14 +10,30 @@
 ?>
 
 <div id="topimg"><img src="images/fotogalleri_top.png" alt="" class="center-block img-responsive" /></div>
-        <header>
-            <h1>Fotogalleri</h1>
-        </header>
-        <section>
-            <button id="back" type="button" class="btn btn-primary">Tilbage til galleri</button>
-            <div id="galleri"></div>
-            <div id="image"></div>
-        </section>
+    <header>
+        <h1>Fotogalleri</h1>
+    </header>
+    <section>
+        <div class="container" style="padding-top: 60px">
+          <div class="row portfolio" id="galleri">
+
+          </div>
+          <!-- Modal -->
+          <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                  <h4 class="modal-title" id="myModalLabel">Im the awesome cat!</h4>
+                </div>
+                <div class="modal-body">
+                  <img class="img-responsive" src="http://lorempixel.com/600/400/cats/?42" alt="The awesome description">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </section>
 <noscript>Fotogalleriet kr&aelig;ver javascript</noscript>
 <?php
     $doc->prtCommonScripts();
